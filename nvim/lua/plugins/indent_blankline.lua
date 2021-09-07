31cdd80d1fdae9219362c2f4a4ba046a2666955d
Brandon Siders <tyrannicaltoucan@gmail.com>
2021-09-07 15:09:03 -0500

    Display first indent level

--- nvim/lua/plugins/indent_blankline.lua
+++ nvim/lua/plugins/indent_blankline.lua
@@ -1,6 +1,5 @@
 require("indent_blankline").setup {
     char = "▏",
-    show_first_indent_level = false,
     show_trailing_blankline_indent = false,
     use_treesitter = true,
     filetype_exclude = { "help", "gitcommit", "text", "packer" },
