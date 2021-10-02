aa3e7ef94dd5d6a9fde21b3ef2bcad7a0e00347b
Brandon Siders <tyrannicaltoucan@gmail.com>
2021-10-02 16:30:33 -0500

    Add platform checks for macOS-specific actions
    This will allow for the addition of configs and actions for specific
    platforms. These checks are temporary until the Linux configs are added.

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -7,3 +7,19 @@ action_msg () {
 is_installed() {
     command -v "$1" >/dev/null
 }
+
+system_type() {
+    local system=""
+    local kernel=""
+
+    kernel="$(uname -s)"
+    if [ "$kernel" == "Darwin" ]; then
+        system="mac"
+    elif [ "$kernel" == "Linux" ] && is_installed "lsb_release"; then
+        system="$(lsb_release -is | tr '[:upper:]' '[:lower:]')"
+    else
+        system="unknown"
+    fi
+
+    echo "$system"
+}
