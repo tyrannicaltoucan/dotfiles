a42439fe037a85eb35413e0f04ed9acd78aa8308
Brandon Siders <tyrannicaltoucan@gmail.com>
2021-10-02 16:03:36 -0500

    Remove obsolete utility functions

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -4,15 +4,6 @@ info_message() {
     printf "$(tput bold)$(tput setaf 6)$@$(tput sgr0)\n"
 }
 
-error_message() {
-    printf "$(tput bold)$(tput setaf 1)error: $@$(tput sgr0)\n"
-}
-
-link() {
-    printf "$(tput setaf 2)$1$(tput sgr0) → $(tput setaf 2)$2$(tput sgr0)\n"
-    ln -snf "$1" "$2"
-}
-
 installed() {
     if command -v "$1" >/dev/null; then
         return 0
