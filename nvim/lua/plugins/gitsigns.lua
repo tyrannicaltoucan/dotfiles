72605fcb7fa3071eb8195d087f18f5e9a36f4f26
Brandon Siders <tyrannicaltoucan@gmail.com>
2021-12-14 19:57:45 -0600

    Change gitsigns' hunking keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,12 +2,12 @@ require('gitsigns').setup {
     max_file_length = 1500,
     keymaps = {
         buffer = true,
-        ['n <leader>g]'] = [[<cmd>lua require('gitsigns').next_hunk({wrap = true})<cr>zz]],
-        ['n <leader>g['] = [[<cmd>lua require('gitsigns').prev_hunk({wrap = true})<cr>zz]],
-        ['n <leader>g+'] = [[<cmd>lua require('gitsigns').stage_hunk()<cr>]],
-        ['n <leader>g-'] = [[<cmd>lua require('gitsigns').reset_hunk()<cr>]],
-        ['n <leader>gp'] = [[<cmd>lua require('gitsigns').preview_hunk()<cr>]],
-        ['n <leader>gb'] = [[<cmd>lua require('gitsigns').blame_line()<cr>]],
-        ['n <leader>gr'] = [[<cmd>lua require('gitsigns').refresh()<cr>]]
+        ['n <leader>h]'] = [[<cmd>lua require('gitsigns').next_hunk({wrap = true})<cr>zz]],
+        ['n <leader>h['] = [[<cmd>lua require('gitsigns').prev_hunk({wrap = true})<cr>zz]],
+        ['n <leader>h+'] = [[<cmd>lua require('gitsigns').stage_hunk()<cr>]],
+        ['n <leader>h-'] = [[<cmd>lua require('gitsigns').reset_hunk()<cr>]],
+        ['n <leader>hp'] = [[<cmd>lua require('gitsigns').preview_hunk()<cr>]],
+        ['n <leader>hb'] = [[<cmd>lua require('gitsigns').blame_line()<cr>]],
+        ['n <leader>hr'] = [[<cmd>lua require('gitsigns').refresh()<cr>]]
     }
 }
