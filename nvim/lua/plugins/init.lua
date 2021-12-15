9b9bda729d33e413f95a28a94cb126eb0cc90030
Brandon Siders <tyrannicaltoucan@gmail.com>
2021-12-14 20:24:33 -0600

    Fix packer installation bugs

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,15 +1,14 @@
 local fn = vim.fn
 local cmd = vim.cmd
-local packer = require('packer')
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
     cmd('packadd packer.nvim')
 end
 
+local packer = require('packer')
 packer.init {
-    compile_path = fn.stdpath('data') .. '/site/pack/packer/packer_compiled.lua',
+    compile_path = vim.fn.stdpath('data') .. '/site/plugin/packer_compiled.lua',
     display = {
         open_fn = function()
             return require('packer.util').float { border = 'single' }
