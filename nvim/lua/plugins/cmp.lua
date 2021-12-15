27732c1fe49a8d13e480f452dd9cb419fb35c1f6
Brandon Siders <tyrannicaltoucan@gmail.com>
2021-12-14 19:38:28 -0600

    Add support for completion snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,27 +1,44 @@
-local cmp = require("cmp")
+local api = vim.api
+local fn = vim.fn
+local cmp = require('cmp')
+local luasnip = require('luasnip')
 
 cmp.setup {
-    completion = { keyword_length = 2 },
-    documentation = false,
-    formatting = {
-        format = function(entry, vim_item)
-            vim_item.menu =({
-                nvim_lsp = "[LSP]",
-                nvim_lua = "[Lua]",
-                path = "[Path]",
-            })[entry.source.name]
-
-            return vim_item
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
         end
     },
     mapping = {
-        ['<C-y>'] = cmp.mapping.confirm({
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
-            select = true
-        })
+            select = true,
+        },
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                fn.feedkeys(api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+            else
+                fallback()
+            end
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                fn.feedkeys(api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+            else
+                fallback()
+            end
+        end
     },
     sources = {
-        { name = "buffer" },
-        { name = "nvim_lsp" },
-    }
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    },
 }
