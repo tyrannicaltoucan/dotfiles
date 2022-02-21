834bcb81d918a1085c4c58f8b80c1156fdae602a
Brandon Siders <tyrannicaltoucan@gmail.com>
2022-02-21 17:38:37 -0600

    Move symlink function to `utilities.sh`

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -9,15 +9,6 @@ config_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
 dotfiles_dir="$(cd "$(dirname "$0")" && pwd -P)"
 mkdir -p "$config_dir"
 
-create_link() {
-    if [ -L "$2" ] && [ -e "$2" ]; then
-        echo "Symlink '$2' already exists!"
-    else
-        echo "$1 → $2"
-        ln -sn "$1" "$2"
-    fi
-}
-
 create_link "$dotfiles_dir/nvim" "$config_dir/nvim"
 create_link "$dotfiles_dir/tmux" "$config_dir/tmux"
 create_link "$dotfiles_dir/git/gitconfig" "$HOME/.gitconfig"
--- setup/utilities.sh
+++ setup/utilities.sh
@@ -12,6 +12,15 @@ is_installed() {
     command -v "$1" >/dev/null
 }
 
+create_link() {
+    if [ -L "$2" ] && [ -e "$2" ]; then
+        echo "Symlink '$2' already exists!"
+    else
+        echo "$1 → $2"
+        ln -sn "$1" "$2"
+    fi
+}
+
 system_type() {
     local system="unknown"
     if [ "$(uname -s)" == "Darwin" ]; then
