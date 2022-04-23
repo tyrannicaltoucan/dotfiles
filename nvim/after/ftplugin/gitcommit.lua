471464fbb874cd24bef37c73ad74960be91ef718
Brandon Siders <tyrannicaltoucan@gmail.com>
2022-04-23 13:46:45 -0500

    Reimplement filetype settings as `ftplugin`s again

---
+++ nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,3 @@
+vim.opt_local.colorcolumn = { 51, 73 }
+vim.opt_local.list = false
+vim.opt_local.spell = true
---
+++ nvim/after/ftplugin/json.lua
@@ -0,0 +1,3 @@
+vim.set_local.shiftwidth = 2
+vim.set_local.tabstop = 2
+vim.set_local.softtabstop= 2
