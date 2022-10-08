91fcb32933b0946ebdb5162778e5a9e836f5ad70
Brandon Siders <tyrannicaltoucan@gmail.com>
2022-10-08 17:41:44 -0500

    Add mappings for gitsigns hunking

--- nvim/lua/plugin/gitsigns.lua
+++ nvim/lua/plugin/gitsigns.lua
@@ -6,4 +6,13 @@ require("gitsigns").setup {
     topdelete = { hl = "GitSignsDelete", text = "‣" },
     changedelete = { hl = "GitSignsChangeDelete", text = "┃" },
   },
+  on_attach = function(bufnr)
+    local map = require("helpers").map
+    local gitsigns = package.loaded.gitsigns
+
+    map({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+    map({"n", "v"}, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+    map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+    map("n", "<leader>hp", gitsigns.preview_hunk)
+  end
 }
